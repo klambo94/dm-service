1d211ca011630660f884d8d9e91aaed95d8b68f8
Kendra Lamb <klambo94@gmail.com>
2024-08-02 12:26:23 -0400

    initial commit of project files

---
+++ src/main/java/com/klambo94/dm_service/DmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.klambo94.dm_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klambo94/dm_service/DmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klambo94.dm_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
